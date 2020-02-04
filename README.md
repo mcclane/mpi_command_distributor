9d18cce0fb06bed32bba153061d24de80abf521d
McClane Howland <mcclane.howland@gmail.com>
2020-02-03 19:38:08 -0800

    stealing some functions from the existing MPiDock project

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # Message Passing Interface (MPI) command distributor
+
 Distributes commands in chunks to be run on an MPI implementing cluster and retrieves the return codes of the commands. This is intended to be simple and generic.
 
+
 ## Why?
+
 This is being developed to screen about 7 million potential malaria drugs on Cal Poly
 s MPAC and DIRAC clusters.
 
+
 ## Requirements
+
 - A cluster with an MPI implementation installed. This is currently being developed on the MPAC cluster with OpenMPI installed.
 - python3
     + mpi4py
 
+
 ## Running a local example
 
 `mpiexec -n 4 python3.4 test.py`
+
+
+## The big daddy run distributed command
+
+`mpiexec --mca btl_tcp_if_include ens6f0 --prefix /usr/lib64/openmpi --hostfile hostfile python3.4 test.py commands.txt`
---
+++ command_distributor.py
@@ -0,0 +1,91 @@
+from mpi4py import MPI
+import numpy as np
+import os
+import sys
+from socket import gethostname
+import time
+
+COMMAND_CHUNK_SIZE = 5
+
+def main():
+    rank = MPI.COMM_WORLD.Get_rank()
+    if len(sys.argv) < 2:
+            print("usage: mpiexec ... python3.4 <file with list of commands>")
+            return
+    if rank == 0:
+        m = Manager({
+            'command_file': sys.argv[1]
+        })
+        m.manage()
+    else:
+        worker()
+
+
+class Manager:
+    def __init__(self, args):
+        self.args = args
+
+    def process_return_codes(self, received):
+        print("Return codes received from", received['hostname'], received['return_codes'])
+        
+    def stop_workers(self):
+        status = MPI.Status()
+        print("Sending out stop signals")
+        for i in range(MPI.COMM_WORLD.Get_size() - 1):
+            received = MPI.COMM_WORLD.recv(status=status)
+            if 'return_codes' in received:
+                self.process_return_codes(received)
+            MPI.COMM_WORLD.send({
+                    'stop'
+                },
+                dest=status.Get_source())
+        print("Master done")
+
+    def manage(self):
+        status = MPI.Status()
+        with open(self.args['command_file']) as command_file:
+            nextline = command_file.readline()
+            while nextline:
+                received = MPI.COMM_WORLD.recv(status=status)
+                print("received ", received, "from", status.Get_source())
+                if 'return_codes' in received:
+                    self.process_return_codes(received)
+
+                commands_to_send = []
+                i = 0
+                while i < COMMAND_CHUNK_SIZE and nextline:
+                    commands_to_send.append(nextline.strip())
+                    nextline = command_file.readline()
+                    i += 1
+
+                MPI.COMM_WORLD.send({
+                        'commands': commands_to_send
+                    }, 
+                    dest=status.Get_source()
+                )
+        self.stop_workers()
+
+        
+def worker():
+    hostname = gethostname()
+    MPI.COMM_WORLD.send({'ready'}, dest=0)
+    while True:
+        received = MPI.COMM_WORLD.recv(source=0)
+        if 'commands' in received:
+            results = []
+            for cmd in received['commands']:
+                return_code = os.system(cmd)
+                results.append([cmd, return_code])
+            MPI.COMM_WORLD.send({
+                    'return_codes': results,
+                    'hostname': hostname
+                }, 
+                dest=0)
+        elif 'stop' in received:
+            break
+        time.sleep(1)
+
+
+
+if __name__ == '__main__':
+    main()
